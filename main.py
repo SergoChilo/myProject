576836a6c3a824cc31a5bd8b2fadc7760660805a
Sergey <90551867+SergoChilo@users.noreply.github.com>
2024-04-06 20:46:27 +0400

    first step

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from PyQt5 import QtWidgets, uic
 import sys
-
-from PyQt5.QtWidgets import QMainWindow, QApplication, QGraphicsScene, QGraphicsView
-import pyqtgraph as pg
-from PyQt5.QtGui import QBrush, QPen, QPainter
-from PyQt5.QtCore import Qt
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-from matplotlib.figure import Figure
+from PyQt5.QtWidgets import QMainWindow, QApplication
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -17,41 +11,60 @@ from sklearn.linear_model import LinearRegression
 class MyWindow(QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
+        # upload our main interface
         uic.loadUi('untitled.ui', self)
 
+        ### CONNECTIONS ==========================================================
         self.exit_app.clicked.connect(self.button_clicked)
-        self.countButton.clicked.connect(self.uiComponents)
-
-        self.showGraphButton.hide()
+        self.buildButton.clicked.connect(self.buildModel)
         self.showGraphButton.clicked.connect(self.paintMyGraphic)
 
+        ### Buttons behavior (visible(default) / Hidden) =========================
+        self.showGraphButton.hide()
+
+        ### Combo Boxes behavior
+        self.exchangeBox1.activated[str].connect(self.onChanged1)
+        self.exchangeBox2.activated[str].connect(self.onChanged2)
+
+        # global value1, value2
+        #
+        # value1 = self.onChanged1(self.exchangeBox1.currentText())
+        # value2 = self.onChanged2(self.exchangeBox2.currentText())
+
+        ### Functional of buttons ================================================
+        # print(value1)
+        # print(value2)
     def button_clicked(self):
         exit()
 
-    def uiComponents(self):
-        self.showGraphButton.show()
+    def onChanged1(self, text):
+        print(text)
 
-        dataset = pd.read_csv('Sources/Salary_Data.csv')
-        X = dataset.iloc[:, :-1].values
-        y = dataset.iloc[:, -1].values
+    def onChanged2(self, text):
+        print(text)
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3, random_state=0)
 
-        regressor = LinearRegression()
-        regressor.fit(X_train, y_train)
+    def buildModel(self):
+        value1 = self.exchangeBox1.currentText()
+        value2 = self.exchangeBox2.currentText()
+        if value1 != value2:
 
-        y_pred = regressor.predict(X_test)
-        plt.scatter(X_train, y_train, color='red')
-        plt.plot(X_train, regressor.predict(X_train),
-                 color='blue')
-        plt.title('Salary vs Experience (Training set)')
-        plt.xlabel('Years of Experience')
-        plt.ylabel('Salary')
+            dataset = pd.read_csv(f'Sources/{value1}_{value2} Historical Data.csv')
 
+            plt.scatter(dataset["Date"], dataset["Price"], color='red')
+            plt.title('Changing AMD to USD')
+            plt.xlabel('Data')
+            plt.ylabel('AMD')
+            self.buildButton.setText("Done")
+            self.showGraphButton.show()
+        else:
+            pass
     def paintMyGraphic(self):
         plt.show()
         self.showGraphButton.hide()
+        self.buildButton.setText("Build model")
 
+    ### Functional of Combo Boxes ================================================
 
 
 def window():
